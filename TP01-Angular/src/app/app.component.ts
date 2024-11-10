d1556cce53c800c0ea8b74e3c14f5a8d58f0c264
HeyEberth <60485498+Eberth-sys@users.noreply.github.com>
2024-11-10 00:24:01 -0300

    Boton de ver detalles
    Agrego boton mostrar detalles de un dispositivo

--- TP01-Angular/src/app/app.component.ts
+++ TP01-Angular/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ListaDispositivosComponent } from './components/lista-dispositivos/lista-dispositivos.component';
+import { DetalleDispositivoComponent } from './components/detalle-dispositivo/detalle-dispositivo.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ListaDispositivosComponent],
+  imports: [RouterOutlet, ListaDispositivosComponent, DetalleDispositivoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- TP01-Angular/src/app/components/lista-dispositivos/lista-dispositivos.component.ts
+++ TP01-Angular/src/app/components/lista-dispositivos/lista-dispositivos.component.ts
@@ -2,19 +2,21 @@ import { Component, OnInit } from '@angular/core';
 import { DispositivoService } from '../../services/dispositivo.service'; //Importo el servicio de dispotivos.
 import { Dispositivo } from '../../interfaces/dispositivo';  //Importo Dispositivos 
 import { CommonModule } from '@angular/common'; //Importo CommonModulo para el uso de ngfor
+import { DetalleDispositivoComponent } from '../detalle-dispositivo/detalle-dispositivo.component';  //importo el componente del detalles del dispotivos.
 
 
 
 @Component({
   selector: 'app-lista-dispositivos',
   standalone: true,
-  imports: [CommonModule], //Importo CommoModule.
+  imports: [CommonModule, DetalleDispositivoComponent], //Importo
   templateUrl: './lista-dispositivos.component.html',
   styleUrl: './lista-dispositivos.component.css'
 })
 export class ListaDispositivosComponent {
 
   dispositivos: Dispositivo[] = []; //Donde alamcenaré la lista de dispotivos. 
+  dispositivoSeleccionado!: Dispositivo | null; //Almaceno el dispositivo sleccionado.
 
   constructor(private dispositivoService: DispositivoService) {} //Contructuro para permiti acceder al servicio dentro del componente.
 
@@ -22,4 +24,10 @@ export class ListaDispositivosComponent {
   ngOnInit(): void { 
     this.dispositivos = this.dispositivoService.obtenerDispositivos(); //funcion para obtner la lista
   }
+
+
+  // Método para seleccionar un dispositivo
+  seleccionarDispositivo(dispositivo: Dispositivo): void {
+    this.dispositivoSeleccionado = dispositivo;
+}
 }
